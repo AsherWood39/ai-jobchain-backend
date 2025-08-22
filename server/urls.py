051d7af3bc8923bc2f2e8940d3442992726cac8e
AsherWood39 <athishtapa05@gmail.com>
2025-08-22 23:28:32 +0530

    Integrated the AI filtering technique to the Backend

--- server/urls.py
+++ server/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from jobs.views import JobPostViewSet, me, set_role
+from jobs.views import JobPostViewSet, me, set_role, AIRecommendationsView
 
 router = DefaultRouter()
 router.register('jobs', JobPostViewSet, basename='job')
@@ -11,4 +11,5 @@ urlpatterns = [
 	path('api/', include(router.urls)),
 	path('api/me', me),
 	path('api/set-role', set_role),
+  path('ai/recommendations/', AIRecommendationsView.as_view(), name='ai-recommendations'),
 ]
